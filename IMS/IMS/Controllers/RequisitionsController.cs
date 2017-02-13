d1e2738fe07ffc287a21c42bc19e48c458fa21d2
Shahbaz <shahbazaliengr@gmail.com>
2017-02-13 23:29:52 +0500

    Adding Requisitions

---
+++ IMS/IMS/Controllers/RequisitionsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IMS;
+
+namespace IMS.Controllers
+{
+    public class RequisitionsController : Controller
+    {
+        private IMSContext db = new IMSContext();
+
+        // GET: Requisitions
+        public ActionResult Index()
+        {
+            var requisitions = db.Requisitions.Include(r => r.Department).Include(r => r.Product);
+            return View(requisitions.ToList());
+        }
+
+        // GET: Requisitions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisition requisition = db.Requisitions.Find(id);
+            if (requisition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(requisition);
+        }
+
+        // GET: Requisitions/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName");
+            return View();
+        }
+
+        // POST: Requisitions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RequisitionId,DepartmentId,ProductId,Quantity,Reason,RequisitionDate,Ordered")] Requisition requisition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Requisitions.Add(requisition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", requisition.DepartmentId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", requisition.ProductId);
+            return View(requisition);
+        }
+
+        // GET: Requisitions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisition requisition = db.Requisitions.Find(id);
+            if (requisition == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", requisition.DepartmentId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", requisition.ProductId);
+            return View(requisition);
+        }
+
+        // POST: Requisitions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RequisitionId,DepartmentId,ProductId,Quantity,Reason,RequisitionDate,Ordered")] Requisition requisition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(requisition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", requisition.DepartmentId);
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", requisition.ProductId);
+            return View(requisition);
+        }
+
+        // GET: Requisitions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Requisition requisition = db.Requisitions.Find(id);
+            if (requisition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(requisition);
+        }
+
+        // POST: Requisitions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Requisition requisition = db.Requisitions.Find(id);
+            db.Requisitions.Remove(requisition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
